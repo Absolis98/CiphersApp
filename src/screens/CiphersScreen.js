edcb1b0246edb9d4386de19fd441e173e35fa9a5
daniel-c-rodriguez <daniel.c.rodriguez@students.tamuk.edu>
2021-02-20 21:47:06 -0600

    Ciphers Screen style update

--- src/screens/CiphersScreen.js
+++ src/screens/CiphersScreen.js
@@ -8,38 +8,48 @@ import {
   FlatList,
 } from "react-native";
 import CipherButton from "../components/CipherButton";
+import { LinearGradient } from "expo-linear-gradient";
 
 const CiphersScreen = ({ navigation }) => {
   const screenList = [{ key: "1" }, {}];
 
   return (
     <View>
-      <Text>Hello there!</Text>
-      <Text>Hello there!</Text>
-      <Text>CiphersScreen</Text>
-      <View style={styles.buttonContainer}>
-        {/* <CipherButton title="K-Rail Fence Cipher"/> */}
-        <Button
-          title="K-Rail Fence Cipher"
-          onPress={() => navigation.push("CipherTabScreen")}
-        />
-      </View>
+      <Text style={styles.Text}>
+        CiphersScreen: (More ciphers to come soon)
+      </Text>
+      {/* <CipherButton title="K-Rail Fence Cipher"/> */}
+      <TouchableOpacity onPress={() => navigation.push("CipherTabScreen")}>
+        <LinearGradient
+          style={styles.button}
+          colors={["#43C6AC", "#191654"]}
+          start={[0, 0]}
+          end={[1, 1]}
+        >
+          <Text style={styles.buttonText}>K-Rail Fence Cipher</Text>
+        </LinearGradient>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    flexDirection: "row",
-    backgroundColor: "yellow",
-    alignSelf: "baseline",
+  Text: {
+    marginLeft: 20,
   },
   button: {
-    flex: 1,
-    backgroundColor: "purple",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    marginVertical: 10,
+    backgroundColor: "#00BCD4",
+    borderRadius: 10,
+    borderWidth: 1,
   },
-  buttonTitle: {
-    color: "pink",
+  buttonText: {
+    color: "white",
+    textAlign: "center",
   },
 });
 
